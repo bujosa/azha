28c080ea1db187d59bd66cc484861ddaa083a332
bujosa <davidbujosa@gmail.com>
2023-10-01 16:56:29 +0200

    feat: integrate the transmitter

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import prompt
 import constants
+import transmitter
 
 app = Flask(__name__)
 
@@ -15,15 +16,16 @@ def webhook_whatsapp():
 
     data=request.get_json()
     message=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+    receiver=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
 
     if message is not None:
         res = prompt.get_response(message)
         res=res.replace("\\n","\\\n")
         res=res.replace("\\","")
-        f = open("text.txt", "w")
-        f.write(res)
-        f.close()
-
+        # f = open("text.txt", "w")
+        # f.write(res)
+        # f.close()
+        transmitter.send(receiver,res)
         return jsonify({"status": "success"}, 200)
 
 if __name__ == "__main__":
@@ -31,4 +33,3 @@ if __name__ == "__main__":
   
 # idWA=data['entry'][0]['changes'][0]['value']['messages'][0]['id']
 # timestamp=data['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']
-# telephoneNumberClient=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
